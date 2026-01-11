9dff8924258b7c92a28284820574465bd52b7846
Abbas Shaik <shaikabbas919@gmail.com>
2026-01-11 09:21:25 +0530

    Added 'My Projects' and added a Live Demo link

--- Demo1/index.js
+++ Demo1/index.js
@@ -1,3 +1,7 @@
 const res = () => {
-    document.writeln("Your resume has been downloaded")
+    alert("Your resume has been downloaded")
+}
+
+const ChatBot = () =>{
+    window.open("https://interview-chatbot-tau.vercel.app/")
 }
